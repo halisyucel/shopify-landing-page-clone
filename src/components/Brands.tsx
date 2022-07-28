8764b98ce4ac42352b0a5378b30f75fed4c3529d
halisycel <isaycel2001@gmail.com>
2022-07-29 00:51:34 +0300

    add brand section

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Brands: React.FC = () => {
     return (
-        <section className={'center my-8 border-y-[1px] border-gray-300'}>
-            brands
+        <section className={'center my-8 py-12 border-y-[1px] border-gray-300'}>
+            {Array.from({ length: 8 }).map((_, index) => (
+                <img 
+                    key={index}
+                    src={`/brand-${index + 1}.png`}
+                    alt={`Brand ${index + 1}`}
+                    className={'w-[90px] mr-8 last:mr-0'}
+                />
+            ))}
         </section>
     )
 }
